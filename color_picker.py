d67d16cf2130ce0bf36d46a7051d1b307b5847e9
Harley Nador <thegreyfedora@gmail.com>
2019-01-14 16:24:32 +1100

    Added color_picker.py, a tool that shows the hue, saturation and value for the pixel under the mouse.

---
+++ color_picker.py
@@ -0,0 +1,16 @@
+import numpy
+import cv2
+
+img = cv2.imread("C:/Users/User1/Desktop/Vision Images/Retro Tape/Low Exposure/CargoAngledDark48in.jpg")
+
+def printHSV (event, x, y, flags, param):
+    print(img[y, x], x, y)
+
+while True:
+    cv2.namedWindow('image')
+    cv2.setMouseCallback('image', printHSV)
+
+    cv2.imshow('image', img)
+    if (cv2.waitKey(1) & 0xFF == ord('q')):
+        break
+cv2.destroyAllWindows
