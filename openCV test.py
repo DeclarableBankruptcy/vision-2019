87709690ad3f3062c3aef1db54f25a97f3350ddc
Harley Nador <thegreyfedora@gmail.com>
2019-01-14 15:02:14 +1100

    Encapsulated code into a function.

--- openCV test.py
+++ openCV test.py
@@ -3,67 +3,69 @@ import cv2
 import os
 import time
 
-lower_green = np.array([0, 130, 130])
-upper_green = np.array([60, 255, 255])
+lower_green = np.array([60, 177, 177])
+upper_green = np.array([160, 255, 255])
 cap = cv2.VideoCapture(0)
 totalCoordsX = 0
 totalCoordsY = 0
-files = os.listdir('C:/Users/User1/Desktop/Vision Images/')
+files = os.listdir('C:/Users/User1/Desktop/Vision Images/Retro Tape/Low Exposure/')
 index = 0
 sensorLength = 10
 focalLength = 20
-fieldOfView = 60
-screenWidth = 720
+fieldOfView = 68.5
+screenWidth = 320
 acrossRat = 0
 degreesAcross = 0
-while True:
-    file = files[index]
-    img = cv2.imread('C:/Users/User1/Desktop/Vision Images/' + file)
-    # ret, img = cap.read()
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(hsv, lower_green, upper_green)
-    
-    cv2.erode(mask, None, dst=mask, iterations=2)
-    cv2.dilate(mask, None, dst=mask, iterations=2)
 
-    contours = cv2.findContours(mask, 1, 2) [-2]
-    sorted(contours, key=cv2.contourArea)
-    
-    if len(contours) > 2:
-        cnt1 = contours[len(contours) - 1]
-        cnt2 = contours[len(contours) - 2]
+def process (fieldOfView, screenWidth):
+    while True:
+        file = files[index]
+        img = cv2.imread('C:/Users/User1/Desktop/Vision Images/Retro Tape/Low Exposure/' + file)
+        # ret, img = cap.read()
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv, lower_green, upper_green)
         
-    rect1 = cv2.minAreaRect(cnt1)
-    box1 = cv2.boxPoints(rect1)
-    box1 = np.int0(box1)
-    cv2.drawContours(img, [box1], -1, (0, 0, 255), 1)
+        cv2.erode(mask, None, dst=mask, iterations=2)
+        cv2.dilate(mask, None, dst=mask, iterations=2)
 
-    rect2 = cv2.minAreaRect(cnt2)
-    box2 = cv2.boxPoints(rect2)
-    box2 = np.int0(box2)
-    cv2.drawContours(img, [box2], -1, (0, 0, 255), 1)    
+        contours = cv2.findContours(mask, 1, 2) [-2]
+        sorted(contours, key=cv2.contourArea)
+        
+        if len(contours) > 1:
+            cnt1 = contours[len(contours) - 1]
+            cnt2 = contours[len(contours) - 2]
+            
+        rect1 = cv2.minAreaRect(cnt1)
+        box1 = cv2.boxPoints(rect1)
+        box1 = np.int0(box1)
+        cv2.drawContours(img, [box1], -1, (0, 0, 255), 1)
 
-    box1Average = np.average(box1, axis=0)
-    box2Average = np.average(box2, axis=0)
+        rect2 = cv2.minAreaRect(cnt2)
+        box2 = cv2.boxPoints(rect2)
+        box2 = np.int0(box2)
+        cv2.drawContours(img, [box2], -1, (0, 0, 255), 1)    
 
-    centerX = int((box1Average[0] + box2Average[0]) / 2)
-    centerY = int((box1Average[1] + box2Average[1]) / 2)
-    #centerX = box1Average
-    
-    cv2.circle(img, (centerX, centerY), 2, (0, 0, 255), -1)
+        box1Average = np.average(box1, axis=0)
+        box2Average = np.average(box2, axis=0)
 
-    acrossRat = screenWidth / centerX
+        centerX = int((box1Average[0] + box2Average[0]) / 2)
+        centerY = int((box1Average[1] + box2Average[1]) / 2)
+        #centerX = box1Average
+        
+        cv2.circle(img, (centerX, centerY), 2, (0, 0, 255), -1)
 
-    degreesAcross = fieldOfView / acrossRat - 30
+        acrossRat = screenWidth / centerX
 
-    print(degreesAcross)
-    cv2.imshow("IMG", img)
-    cv2.imshow("MASK", mask)
-    if cv2.waitKey(1) & 0xFF == ord('g'):
-        cv2.destroyAllWindows()
-        index += 1
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-    # time.sleep(2)
-cv2.destroyAllWindows()
-cap.release()
+        degreesAcross = fieldOfView / acrossRat - 30
+
+        print(degreesAcross)
+        cv2.imshow("IMG", img)
+        cv2.imshow("MASK", mask)
+        if cv2.waitKey(1) & 0xFF == ord('g'):
+            cv2.destroyAllWindows()
+            index += 1
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        # time.sleep(2)
+    cv2.destroyAllWindows()
+    cap.release()
