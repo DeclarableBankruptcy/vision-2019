cdfb4a376c32066d4e2aaafc30f2b465a735af54
Harley Nador <thegreyfedora@gmail.com>
2019-01-16 19:34:57 +1100

    Fixed a bug in color_picker.py that made it pick values in RGB instead of the preferred HSV

--- Tools/color_picker.py
+++ Tools/color_picker.py
@@ -1,10 +1,14 @@
-import numpy
 import cv2
 
-img = cv2.imread("C:/Users/User1/Desktop/Vision Images/sample1.jpg")
 
-def printHSV (event, x, y, flags, param):
-    print(img[y, x], x, y)
+img = cv2.imread("C:/vision-2019/Tools/Sample Images/CargoLine36in.jpg")
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+
+def printHSV(event, x, y, flags, param):
+    # Print each item in the pixel array
+    print(hsv[y, x], x, y)
+
 
 while True:
     cv2.namedWindow('image')
@@ -12,5 +16,7 @@ while True:
 
     cv2.imshow('image', img)
     if (cv2.waitKey(1) & 0xFF == ord('q')):
+        # Quit the program
         break
+
 cv2.destroyAllWindows
