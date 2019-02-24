2f09844b6fdb30a708f1ce247c5e81a427de47c0
Harley Nador <thegreyfedora@gmail.com>
2019-02-24 11:06:56 +1100

    Work for 12 pins, not just 8.

--- Vision/reflectance_sensors.py
+++ Vision/reflectance_sensors.py
@@ -3,26 +3,29 @@ from datetime import datetime
 
 lower_time = 0
 upper_time = 0
-chan_list = [11, 12, 13, 15, 16, 18, 22, 7]
-left_chan = [11, 12, 13, 15]
-right_chan = [16, 18, 22, 7]
+chan_list = [11, 12, 13, 15, 16, 18, 22, 7, 3, 5, 24, 26]
+left_chan = [11, 12, 13, 15, 16, 18]
+right_chan = [22, 7, 3, 5, 24, 26]
 spacing = 5
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(chan_list, GPIO.OUT, initial=GPIO.HIGH)
 
+
 def track_sensors(channels):
+    activated = []
     for channel in channels:
         old_time = datetime.time.microsecond()
         GPIO.setup(channel, GPIO.IN)
         while GPIO.input(channel) == GPIO.HIGH:
-            time.delay(0.001)
+            datetime.delay(0.001)
         new_time = datetime.time.microsecond()
         elapsed = new_time - old_time
         if elapsed < upper_time and elapsed > lower_time:
             activated.append(channel)
         
     return activated
+
 
 def correct_alignment():
     leftActivated = track_sensors(left_chan)
@@ -31,6 +34,6 @@ def correct_alignment():
         return 0
     elif len(leftActivated) > len(rightActivated):
         return (len(leftActivated) * -spacing) / 100
-    elif len(rightActivated > len(leftActivated):
+    elif len(rightActivated) > len(leftActivated):
         return (len(rightActivated) * spacing) / 100
 
