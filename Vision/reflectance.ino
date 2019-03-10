0d3e1145440812e025a133397c95454ad98cd116
Harley Nador <thegreyfedora@gmail.com>
2019-03-10 20:39:33 +1100

    Add arduino code for reflectance sensors.

---
+++ Vision/reflectance.ino
@@ -0,0 +1,31 @@
+int sensorPin = 7;
+int dataOutPin = 8;
+int threshold = 0;
+bool reading = false;
+int oldTime, newTime, elapsed;
+void setup() {
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(sensorPin, HIGH);
+  delay(1);
+  
+  oldTime = millis();
+  pinMode(7, INPUT);
+  
+  while (digitalRead(7) == HIGH) {
+    
+  }
+  
+  newTime = millis();
+  elapsed = newTime - oldTime;
+  
+  if (elapsed > threshold) {
+    digitalWrite(dataOutPin, HIGH);
+  }
+  else {
+    digitalWrite(dataOutPin, LOW);
+  }
+}
